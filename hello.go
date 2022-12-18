88e5bf117f9f2ff4da1194f9e0ff93bdecf16113
AliAkbarBadri <aabadri74@gmail.com>
2022-12-19 00:13:23 +0330

    add get input

--- hello.go
+++ hello.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func convertFahr2Cel() float64 {
+	fmt.Print("enter fahrenheit:")
+	var input float64
+	fmt.Scanf("%f", &input)
+	output := (input - 32) * 5 / 9
+	return output
+}
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World", 10)
+	fmt.Println(convertFahr2Cel())
 }
---
+++ types.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("salam")
+	fmt.Println("ali"[0]) // 97
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
